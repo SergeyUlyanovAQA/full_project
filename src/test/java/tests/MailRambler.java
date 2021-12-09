74d9a7e90ff650cfae6de58cf50ba4b9ac68f17f
SergeyUlyanovAQA <bigbarabumchic@mail.ru>
2021-12-09 11:46:06 +0400

    Start

--- src/test/java/tests/MailRambler.java
+++ src/test/java/tests/MailRambler.java
@@ -1,18 +1,21 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import static com.codeborne.selenide.Selenide.*;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 public class MailRambler {
 
+
+
     @Test
-    void authorizationWithUI () {
+    void authorizationWithUI() {
         open("https://mail.rambler.ru/");
+        switchTo().frame($("[src*='id.rambler.ru/login-20']"));
         $("#login").setValue("mailForTesting0");
         $("#password").setValue("mailForTesting0").pressEnter();
-
 
     }
 
